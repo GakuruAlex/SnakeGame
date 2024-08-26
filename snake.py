85365cae80ff63dd016d430e00bf8173e4c95fb2
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-26 08:01:14 +0000

    create a new snake with three turtles

--- snake.py
+++ snake.py
@@ -6,6 +6,18 @@ screen.setup(height = 600, width = 600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+def build_snake()-> list:
+    snake  = []
+    snake_coord = [(0,0), (-20, 0), (-40, 0)]
+
+    for coord in snake_coord:
+        new_snake =Turtle(shape="square")
+        new_snake.color("white")
+        new_snake.goto(coord)
+        snake.append(new_snake)
+    return snake
+
+build_snake()
 
 
 
