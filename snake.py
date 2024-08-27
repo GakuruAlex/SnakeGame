f4c21174e2247ec71a83c94ced053797d4b07b5b
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-27 06:09:52 +0000

    build a starting snake

--- snake.py
+++ snake.py
@@ -1,26 +1,22 @@
-from turtle import Screen, Turtle
+from turtle import  Turtle
 
-screen = Screen()
 
-screen.setup(height = 600, width = 600)
-screen.bgcolor("black")
-screen.title("Snake Game")
 
 def build_snake()-> list:
     snake  = []
-    snake_coord = [(0,0), (-20, 0), (-40, 0)]
+    snake_starting_pos = [(0,0), (-20, 0), (-40, 0)]
 
-    for coord in snake_coord:
+    for position in snake_starting_pos:
         new_snake =Turtle(shape="square")
         new_snake.color("white")
-        new_snake.goto(coord)
+        new_snake.penup()
+        new_snake.goto(position)
         snake.append(new_snake)
     return snake
 
-build_snake()
 
 
 
 
 
-screen.exitonclick()
+
