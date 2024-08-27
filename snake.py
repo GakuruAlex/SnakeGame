f9ccbf060c6ceab05a35d6a10dd1e4cefcb56c48
alexgicimo <gakurualexgicimo@gmail.com>
2024-08-27 07:00:00 +0000

    refactor to use classes

--- snake.py
+++ snake.py
@@ -1,18 +1,18 @@
 from turtle import  Turtle
 
-
-
-def build_snake()-> list:
-    snake  = []
+class Snake:
     snake_starting_pos = [(0,0), (-20, 0), (-40, 0)]
+    def build_snake(self)-> list:
+        snake  = []
+        
 
-    for position in snake_starting_pos:
-        new_snake =Turtle(shape="square")
-        new_snake.color("white")
-        new_snake.penup()
-        new_snake.goto(position)
-        snake.append(new_snake)
-    return snake
+        for position in self.snake_starting_pos:
+            new_snake =Turtle(shape="square")
+            new_snake.color("white")
+            new_snake.penup()
+            new_snake.goto(position)
+            snake.append(new_snake)
+        return snake
 
 
 
